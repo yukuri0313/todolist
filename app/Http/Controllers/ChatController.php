74319f6a24403b78b7324e240c2f66cb42197fe5
yukuri0313 <77546393+yukuri0313@users.noreply.github.com>
2021-03-16 02:16:33 +0900

    チャット表示

--- app/Http/Controllers/ChatController.php
+++ app/Http/Controllers/ChatController.php
@@ -5,6 +5,7 @@ use App\Models\Chat;
 use App\Models\Course;
 use App\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class ChatController extends Controller
 {
@@ -15,10 +16,13 @@ class ChatController extends Controller
 
         $course = Course::find($id);
         $course_name = $course->lecture_name;
+
+        $speaker = Auth::id();
         
         return view('chats.chat')->with([
             'coursechats' => $coursechats,
-            'course_name' => $course_name
-            ]);
+            'course_name' => $course_name,
+            'speaker' => $speaker
+            ]); 
     }
 }
--- resources/views/chats/chat.blade.php
+++ resources/views/chats/chat.blade.php
@@ -5,6 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous">
+    <link rel="stylesheet" href="{{ asset('css/style4.css') }}">
 
     <title>チャット画面</title>
 </head>
@@ -21,8 +22,14 @@
                 </div>
                 <div class="card-body">
                 @foreach($coursechats as $coursechat)
-                    {{ $coursechat->user->name }}
-                    {{ $coursechat->statement }}<hr>
+                    <div class="name">{{ $coursechat->user->name }}</div>
+                    @if ( $coursechat->user_id == $speaker )
+                    <div class="mycomment">{{ $coursechat->statement }}</div>  
+                    <div class="time">　　/{{ $coursechat->created_at }}</div>
+                    @else
+                    <div class="statement">{{ $coursechat->statement }}</div>
+                    <div class="time">　　/{{ $coursechat->created_at }}</div>
+                    @endif
                 @endforeach
                 </div>
             </div>
